9c95cd6c18d18304fdbf9a2b70804ce699976104
Tajah Johnson <tajah93@hotmail.com>
2020-08-04 05:40:52 -0500

    Working JavaScript functions (display questions and results)

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-
-var Questions = [
+var qQuestions = [
     {
         question: "1. Which of the following best describes an array in JavaScript?", 
-        answers: { 
+        answers: {
             a: "Variable that can only hold one value",
             b: "Variable that only holds strings",
             c: "Variable that holds multiple values",
@@ -53,12 +49,87 @@ var Questions = [
         },
         correctA: "a"
     }
-]; 
+];
 
 
+function javaScriptQuiz(questions, quizContainer, results, submit) {
+
+  
+    function quizOfficial(questions, quizContainer){
+
+        var output = [];
+        var answers;
+
+        for(var i=0; i<questions.length; i++){
+            
+            
+            answers = [];
+
+            
+            for(letter in questions[i].answers){
+
+                // ...add an html radio button
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    + '</label>'
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        
+        quizContainer.innerHTML = output.join('');
+    } ;   
 
 
+  
+    function quizResults(questions, quizContainer, results){
+      
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+      
+        var userChoice = '';
+        var numCorrect = 0;
+        
+       
+        for(var i=0; i<questions.length; i++) {
 
-function Quiz() (
+           
+            userChoice = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+            
+            
+            if(userChoice===questions[i].correctA){
+                numCorrect++;
+                answerContainers[i].style.color = 'green';
+            }
+            
+            else{
+                answerContainers[i].style.color = 'red';
+            }
+        }
 
-)
+        
+        results.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+
+    
+    submit.onclick = function(){
+        quizResults(questions, quizContainer, results);
+    }
+
+    quizOfficial(questions, quizContainer);
+}
+
+var quizContainer = document.getElementById('quiz');
+var results = document.getElementById('results');
+var submit = document.getElementById('submit');
+
+
+javaScriptQuiz(qQuestions, quizContainer, results, submit);
